a64a38ab69521aa6722fe3f7712f2a78a7846268
Descent098 <Descent098@users.noreply.github.com>
2024-08-01 09:39:14 +0000

    deploy: 49f4518b5416eeb1b5b60ccdad238236bf7258a8

--- server/manifest_Bi1trvsd.mjs
+++
@@ -1,6 +0,0 @@
-import '@astrojs/internal-helpers/path';
-import 'cookie';
-export { m as manifest } from './chunks/_@astrojs-manifest_CsB6R3Vp.mjs';
-import 'kleur/colors';
-import './chunks/astro/server_BNoKQkm8.mjs';
-import 'clsx';
